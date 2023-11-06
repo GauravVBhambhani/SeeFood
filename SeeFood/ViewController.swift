de0a32aef8c7a660e1d6a52a1494c7175e8132da
Gaurav Bhambhani <113461154+GauravVBhambhani@users.noreply.github.com>
2023-11-05 20:55:16 -0500

    Created app which will tell the user if the image they have clicked is of a hotdog or not. Created UIImagePicker to access camera and added UINavigationController to navigate between camera and app page. After camera captures image, the image is displayed on app.

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -6,14 +6,42 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var clickedImage: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera //.photolibrary for accessing photos instead of camera.
+        imagePicker.allowsEditing = false
     }
-
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        
+        if let usersImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            
+            clickedImage.image = usersImage
+            
+        }
+        
+        imagePicker.dismiss(animated: true)
+        
+    }
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true)
+    }
+    
+    
+    
 }
 
